70fe890352b9c65639c0357601079a4b0f287db5
Moviet <snorthfed@gmail.com>
2018-12-26 01:54:55 +0700

    Just radical comment lines

--- src/Hash/Pwsuit.php
+++ src/Hash/Pwsuit.php
@@ -19,35 +19,35 @@ use Moviet\Heavy\Exceptions\EqualsException;
 class Pwsuit
 {
     /**
-    * @param int $cost
-    */
+     * @param int $cost
+     */
     protected static $cost;
 
     /**
-    * @param int $memory
-    */
+     * @param int $memory
+     */
     protected static $memory;
 
     /**
-    * @param int $time
-    */
+     * @param int $time
+     */
     protected static $time;
 
     /**
-    * @param int $thread
-    */
+     * @param int $thread
+     */
     protected static $thread;
 
     /**
-    * Set default
-    */
+     * Set default
+     */
     const DEFAULT = 'Default';
 
     /**
-    * Require minimum PHP 5.6+
-    * 
-    * @throws RuntimeException 
-    */
+     * Require minimum PHP 5.6+
+     * 
+     * @throws RuntimeException 
+     */
     public function __construct()
     {
         if (version_compare(PHP_VERSION, '5.6.0', '<')) {
@@ -56,16 +56,16 @@ class Pwsuit
     }
 
     /**
-    * Create cost length
-    * 
-    * if it doesn't set by default a cost will set to 14
-    * do not set under native default by php
-    * native default by php set to 10
-    * 
-    * @param int $length
-    * @return array
-    * 
-    */
+     * Create cost length
+     * 
+     * if it doesn't set by default a cost will set to 14
+     * do not set under native default by php
+     * native default by php set to 10
+     * 
+     * @param int $length
+     * @return array
+     * 
+     */
     public static function cost(int $length)
     {
         static::$cost[Verbal::DEFAULT_COST] = $length;
@@ -74,16 +74,16 @@ class Pwsuit
     }
 
     /**
-    * Create memory_cost length
-    * 
-    * This is optional by default memory_cost will set to 1666 
-    * do not set under native default by php
-    * native default by php set to 1024
-    * 
-    * @param int $length
-    * @return array
-    * 
-    */
+     * Create memory_cost length
+     * 
+     * This is optional by default memory_cost will set to 1666 
+     * do not set under native default by php
+     * native default by php set to 1024
+     * 
+     * @param int $length
+     * @return array
+     * 
+     */
     public static function memory(int $length)
     {
         static::$memory[Verbal::MEMORY_KEY] = $length;
@@ -92,15 +92,15 @@ class Pwsuit
     }
 
     /**
-    * Create time_cost length
-    * 
-    * This is optional by default time_cost will set to 6 
-    * do not set under native default by php
-    * native default by php set to 2
-    * 
-    * @param int $length
-    * @return array
-    */
+     * Create time_cost length
+     * 
+     * This is optional by default time_cost will set to 6 
+     * do not set under native default by php
+     * native default by php set to 2
+     * 
+     * @param int $length
+     * @return array
+     */
     public function time(int $length)
     {
         static::$time[Verbal::TIME_KEY] = $length;
@@ -109,15 +109,15 @@ class Pwsuit
     }
 
     /**
-    * Create threads length
-    * 
-    * This is optional by default threads will set to 6
-    * do not set under native default by php
-    * native default by php set to 2
-    * 
-    * @param int $length
-    * @return array
-    */
+     * Create threads length
+     * 
+     * This is optional by default threads will set to 6
+     * do not set under native default by php
+     * native default by php set to 2
+     * 
+     * @param int $length
+     * @return array
+     */
     public function thread(int $length)
     {
         static::$thread[Verbal::THREAD_KEY] = $length;
@@ -126,45 +126,45 @@ class Pwsuit
     }
 
     /**
-    * Check attributes operation
-    * 
-    * @return array 
-    */
+     * Check attributes operation
+     * 
+     * @return array 
+     */
     protected static function getLength()
     {
         return isset(static::$cost) ? static::$cost : static::costLength();				
     }
 
     /**
-    * Check attributes for eg. password Argon
-    * 
-    * @return array
-    */
+     * Check attributes for eg. password Argon
+     * 
+     * @return array
+     */
     protected static function getOptions()
     {
         return isset(static::$memory) ? static::hashel() : static::options();
     }
 
     /**
-    * Generate default values if attributes exist
-    * eg. Password Argon
-    * 
-    * @return array
-    * 
-    */
+     * Generate default values if attributes exist
+     * eg. Password Argon
+     * 
+     * @return array
+     * 
+     */
     protected static function hashel()
     {
         return array_merge(static::$memory, static::$time, static::$thread);
     }
 
     /**
-    * Check the current attributes that use for compatible version
-    * if doesn't set will return to default config 
-    * 
-    * @param string $hashmode
-    * @param string $key
-    * @return string
-    */
+     * Check the current attributes that use for compatible version
+     * if doesn't set will return to default config 
+     * 
+     * @param string $hashmode
+     * @param string $key
+     * @return string
+     */
     public static function pwHash($hashmode, $key)
     {		
         $options = ($hashmode !== self::DEFAULT && version_compare(PHP_VERSION, '7.2.0', '>=')) ? static::getOptions() : static::getLength();
@@ -173,15 +173,15 @@ class Pwsuit
     }	
 
     /**
-    * Create new hashed with the old hashed password
-    * and check for compatible php version
-    * if match will produce new hashed
-    * 
-    * @param string $hashmode
-    * @param string $password
-    * @param string $datahash
-    * @return Moviet\Heavy\Exceptions\EqualsException
-    */
+     * Create new hashed with the old hashed password
+     * and check for compatible php version
+     * if match will produce new hashed
+     * 
+     * @param string $hashmode
+     * @param string $password
+     * @param string $datahash
+     * @return Moviet\Heavy\Exceptions\EqualsException
+     */
     public static function pwRehash($hashmode, $password, $datahash)
     {
         $options = ($hashmode !== self::DEFAULT && version_compare(PHP_VERSION, '7.2.0', '>=')) ? static::getOptions() : static::getLength();
@@ -197,20 +197,20 @@ class Pwsuit
     }
 
     /**
-    * Generate default cost length
-    * 
-    * @return array
-    */
+     * Generate default cost length
+     * 
+     * @return array
+     */
     protected static function costLength()
     {
         return [Verbal::DEFAULT_COST => Verbal::DEFAULT_COST_LENGTH];
     }
 
     /**
-    * Generate default memory, time, threads for password Argon
-    *  
-    * @return array
-    */
+     * Generate default memory, time, threads for password Argon
+     *  
+     * @return array
+     */
     protected static function options()
     {
         return [Verbal::MEMORY_KEY => Verbal::DEFAULT_MEMORY_COST, Verbal::TIME_KEY => Verbal::DEFAULT_TIME_COST, 
@@ -218,23 +218,23 @@ class Pwsuit
     }
 
     /**
-    * Generate password verification
-    *  
-    * @param string $password
-    * @param string $hash
-    * @return bool
-    */
+     * Generate password verification
+     *  
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     */
     public static function pwTrust($password, $hash)
     {
         return password_verify($password, $hash);
     }
 
     /**
-    * Generate password information
-    *  
-    * @param string $hash
-    * @return array
-    */
+     * Generate password information
+     *  
+     * @param string $hash
+     * @return array
+     */
     public static function pwInfo($hash)
     {
         return password_get_info($hash);
